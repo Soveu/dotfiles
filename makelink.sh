bbc4c9c96f0c407aae5395919f869159aad41843
Soveu <marx.tomasz@gmail.com>
2020-10-20 18:12:37 +0200

    some update

--- makelink.sh
+++ makelink.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ln -sf $PWD/fish/   $HOME/.config/fish
-ln -sf $PWD/vim/    $HOME/.vim
-ln -sf $PWD/bashrc  $HOME/.bashrc
+ln -sf $PWD/fish/       $HOME/.config/fish
+ln -sf $PWD/vim/        $HOME/.vim
+ln -sf $PWD/bashrc      $HOME/.bashrc
+ln -sf $PWD/alacritty/  $HOME/.config/alacritty
+ln -sf $PWD/nvim        $HOME/.config/nvim
 
